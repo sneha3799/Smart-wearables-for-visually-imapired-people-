901b9fb96d95f2a2793f436df92317df8c4e7f4b
sneha rao <raosneha321@gmail.com>
2020-09-24 18:12:11 +0530

    Pothole and stairs part

---
+++ pothole-stairs/README.md
@@ -0,0 +1,7 @@
+# obj_detection
+
+Deployed Tiny-YOLOv2 and ssd mobilenet on android using tensorflow lite. Around 80 objects can be detected.
+
+<p align="center">
+<img src="images/img1.jpg" width="200" height="400">
+</p> 
---
+++ pothole-stairs/lib/models.dart
@@ -0,0 +1,6 @@
+const String mobilenet = "MobileNet";
+const String ssd = "SSD MobileNet";
+const String yolo = "Tiny YOLOv2";
+const String posenet = "PoseNet";
+const String detector = "Custom";
+const String tmachine = "Recognition";
